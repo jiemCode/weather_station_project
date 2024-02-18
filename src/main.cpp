ba7337fddc8d686f5e50002cc463816869040aa3
jiemCode <dmax6176@gmail.com>
2024-02-18 05:31:07 +0000

    upload for index_g2.html

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,9 @@
 #include <MQ135.h>
 #include <ESP_Mail_Client.h>
 
-
+boolean T_status = true;
+boolean H_status = true;
+boolean A_status = true;
 
 // Replace with your network credentials
 const char* ssid = "CROUS DIAMNIADIO";
@@ -25,8 +27,18 @@ const char* password = "CROUS@UAM";
 #define DHTPIN 5     // Digital pin connected to the DHT sensor
 #define PIN_MQ135 35
 
+#define temp_threshold 37
+#define hum_threshold 30
+#define aq_threshold 50
+
+float temperature_v = 0;
+float humidity_v = 0;
+float air_quality_v = 0;
+
+
 // Uncomment the type of sensor in use:
 #define DHTTYPE    DHT11     // DHT 11
+
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -113,6 +125,53 @@ String processor(const String& var){
   return String();
 }
 
+void send_mail_alert(String msg_subject, String msg_content){
+
+  ESP_Mail_Session session;
+
+  session.server.host_name = SMTP_server ;
+
+  session.server.port = SMTP_Port;
+
+  session.login.email = sender_email;
+
+  session.login.password = sender_password;
+
+  session.login.user_domain = "";
+
+  /* Declare the message class */
+
+  SMTP_Message message;
+
+  message.sender.name = "";
+
+  message.sender.email = sender_email;
+
+  message.subject = msg_subject;
+
+  message.addRecipient(Recipient_name,Recipient_email);
+
+   //Send HTML message
+
+  String htmlMsg = msg_content;
+
+  message.html.content = htmlMsg.c_str();
+
+  message.text.charSet = "us-ascii";
+
+  message.html.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
+
+
+  if (!smtp.connect(&session))
+
+    return;
+  
+  if (!MailClient.sendMail(&smtp, &message))
+
+    Serial.println("Error sending Email, " + smtp.errorReason());
+ 
+}
+
 void setup(){
 
   // Serial port for debugging purposes
@@ -150,12 +209,10 @@ void setup(){
   Serial.println();
 
   // Print ESP32 Local IP Address
-  Serial.println(WiFi.localIP());
+  Serial.print("IP Address : ");
+  Serial.print(WiFi.localIP());
 
-  // Route for root / web page
-  // server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-  //   request->send_P(200, "text/html", index_html, processor);
-  // });
+
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
   {
     request->send(SPIFFS, "/index.html", "text/html");
@@ -173,60 +230,33 @@ void setup(){
 
   // Start server
   server.begin();
+  Serial.println("Follow http://" + WiFi.localIP());
 
   Serial.begin(115200);
-
   Serial.println("");
 
   // smtp.debug(1);
 
-
-
-  ESP_Mail_Session session;
-
-  session.server.host_name = SMTP_server ;
-
-  session.server.port = SMTP_Port;
-
-  session.login.email = sender_email;
-
-  session.login.password = sender_password;
-
-  session.login.user_domain = "";
-
-  /* Declare the message class */
-
-  SMTP_Message message;
-
-  message.sender.name = "ESP 32";
-
-  message.sender.email = sender_email;
-
-  message.subject = "ESP32 Testing Email";
-
-  message.addRecipient(Recipient_name,Recipient_email);
-
-   //Send HTML message
-
-  String htmlMsg = "<div style=\"color:#000000;\"><h1> Hello Semicon!</h1><p> Mail Generated from ESP32</p></div>";
-
-  message.html.content = htmlMsg.c_str();
-
-  message.text.charSet = "us-ascii";
-
-  message.html.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
-
-
-  if (!smtp.connect(&session))
-
-    return;
-  
-  if (!MailClient.sendMail(&smtp, &message))
-
-    Serial.println("Error sending Email, " + smtp.errorReason());
-  
+ 
 }
  
 void loop(){
-  
+  temperature_v = dht.readTemperature();
+
+  if (T_status==true && !isnan(temperature_v) && temperature_v > temp_threshold){
+    send_mail_alert("Alerte T", "Temperature elevee !");
+    T_status = false;
+  }
+  humidity_v=dht.readHumidity();
+  if (H_status==true && !isnan(humidity_v) && humidity_v > hum_threshold){
+    send_mail_alert("Alerte H", "Basse !");
+    H_status=false;
+  }
+  MQ135 air_quality = MQ135(PIN_MQ135);
+  air_quality_v = air_quality.getPPM(); 
+  if (A_status==true && !isnan(air_quality_v) && air_quality_v > aq_threshold){
+    send_mail_alert("Alerte AQ", "Mauvaise qualite de l'air!");
+    A_status=false;
+  }
+  delay(500000);
 }
